1744d8d5dc4727d7cd921e91b15e8f4fce2ae1a0
keseho <yoosharaza29@gmail.com>
2024-01-13 15:36:44 +0530

    update env name

--- components/shared/leftsidebar/RightSideBar.tsx
+++ components/shared/leftsidebar/RightSideBar.tsx
@@ -89,7 +89,7 @@ const RightSidebar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
